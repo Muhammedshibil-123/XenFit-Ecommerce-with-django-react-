dee4dad727dd1a0e8336d63bcb2fac265b98c598
Muhammedshibil-123 <127181540+Muhammedshibil-123@users.noreply.github.com>
2025-12-08 13:35:16 +0530

    d

--- BackEnd/users_app/serializers.py
+++ BackEnd/users_app/serializers.py
@@ -26,21 +26,26 @@ class CustomTokenJwtSerializer(TokenObtainPairSerializer):
     
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True) 
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password', 'mobile', 'age']
+        fields = ('username', 'email', 'mobile', 'password', 'confirm_password')
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            mobile=validated_data.get('mobile', ''),
-            age=validated_data.get('age', None)
-        )
+        validated_data.pop('confirm_password')
+       
+        password = validated_data.pop('password')
+        user = CustomUser(**validated_data)
+        user.set_password(password)
         user.is_active = False 
         user.save()
+        
         return user
 
 
