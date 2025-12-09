f6a756915a31ab50adc6401a8f20b056e707c0d5
Muhammedshibil-123 <127181540+Muhammedshibil-123@users.noreply.github.com>
2025-12-09 10:15:29 +0530

    d

--- BackEnd/products/admin.py
+++ BackEnd/products/admin.py
@@ -5,22 +5,15 @@ from .models import Product, ProductSize, ProductImage
 class ProductSizeInline(admin.TabularInline):
     model = ProductSize
     extra = 1  
-
-
 class ProductImageInline(admin.TabularInline):
     model = ProductImage
     extra = 1
-
-
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'product_code', 'color', 'price', 'brand', 'total_stock')
     list_filter = ('brand', 'theme', 'color', 'sleeve_type')
     search_fields = ('title', 'product_code', 'brand')
     
-    
     inlines = [ProductSizeInline, ProductImageInline]
-
-    
     def total_stock(self, obj):
         return sum(item.stock for item in obj.sizes.all())
 
